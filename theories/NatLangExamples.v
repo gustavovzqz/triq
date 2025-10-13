76c3ba30ff0f9fc40b6caadfeef84067ca1f9c25
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2025-10-13 15:54:50 -0300

    exemplo simples de parcialmente computável

--- theories/NatLangExamples.v
+++ theories/NatLangExamples.v
@@ -33,7 +33,7 @@ Definition id_prg :=
 
 
 (**  O programa para com o número 8 como entrada *)
-Theorem id_prg_halts : exists (n : nat), HALT (create_state [8]) id_prg n.
+Theorem id_prg_halts : exists (n : nat), HALT (create_state 8) id_prg n.
 Proof.
   intros. exists 43.
   cbv (* usar o reflexivity antes pode causar problemas *).  reflexivity.
@@ -50,18 +50,18 @@ Qed.
 
 Theorem id_prg_halts' : forall (x0 : nat) (y0 : nat) (z0 : nat),
   exists (t : nat), HALT
-  (t_update (t_update (t_update empty (X 0) x0) (Z 0) z0) Y y0) id_prg t.
+  (update (update (update empty (X 0) x0) (Z 0) z0) Y y0) id_prg t.
 Proof.
   intros x0. unfold HALT. induction x0 as [| m]; intros.
   - exists 3. destruct z0; destruct y0; reflexivity.
   - destruct IHm with (y0 + 1) (z0 + 1). exists (5 + x). 
     simpl compute_program. 
-   assert ((t_update (t_update (t_update empty (X 0) m) (Z 0) (z0 + 1)) Y
-              (y0 + 1)) = ((t_incr (t_incr (t_decr (t_update 
-              (t_update (t_update empty (X 0) (S m)) (Z 0) z0) Y y0) (X 0)) Y)
+   assert ((update (update (update empty (X 0) m) (Z 0) (z0 + 1)) Y
+              (y0 + 1)) = ((incr (incr (decr (update 
+              (update (update empty (X 0) (S m)) (Z 0) z0) Y y0) (X 0)) Y)
               (Z 0)))).
-      { apply functional_extensionality. intros x0. unfold t_incr. 
-        unfold t_decr. unfold t_update. rewrite eqb_var_refl. simpl.
+      { apply functional_extensionality. intros x0. unfold incr. 
+        unfold decr. unfold update. rewrite eqb_var_refl. simpl.
         destruct x0; try (reflexivity); destruct n; try (reflexivity).
         + rewrite PeanoNat.Nat.sub_0_r. reflexivity. }
       rewrite <- H0. destruct z0; assumption.
@@ -70,13 +70,13 @@ Qed.
 (** Agora posso escrever a versão original *)
 
 Theorem id_prg_halts'' : forall (x0 : nat),
-  exists (t : nat), HALT (create_state [x0])
+  exists (t : nat), HALT (create_state x0)
    id_prg t.
 Proof.
   intros x0. unfold create_state. 
-  assert ((t_update empty (X 0) x0) = 
-  (t_update (t_update (t_update empty (X 0) x0) (Z 0) 0) Y 0)). 
-  { apply functional_extensionality. intros x. unfold t_update. unfold empty.
+  assert ((update empty (X 0) x0) = 
+  (update (update (update empty (X 0) x0) (Z 0) 0) Y 0)). 
+  { apply functional_extensionality. intros x. unfold update. unfold empty.
     destruct (eqb_var (X 0) x) eqn:E;
     destruct (eqb_var Y x) eqn:E2; 
     destruct (eqb_var (Z 0) x) eqn:E3; try(reflexivity).
@@ -88,8 +88,30 @@ Proof.
   rewrite H. apply id_prg_halts'.
 Qed.
 
-Theorem id_prg_halts_1000 : exists (t : nat), HALT (create_state [1000])
+Theorem id_prg_halts_1000 : exists (t : nat), HALT (create_state 1000)
   id_prg t.
 Proof.
   apply id_prg_halts''.
+Qed.
+
+
+(** f(x) = 1 é Parcialmente Computável *)
+
+
+Definition f (x : nat) := Some 1.
+
+
+Definition prog := 
+  <{[[] Y <- + 1]}>.
+
+
+
+Theorem f_part_comp : partially_computable f.
+Proof.
+  unfold partially_computable. exists prog. intros x0. unfold f.
+  split.
+  + intros H. discriminate H.
+  + intros H. exists 2. split.
+    ++ unfold HALT. simpl. reflexivity.
+    ++ unfold get_Y. simpl. reflexivity.
 Qed.
