f071bae2f53046e291499ff16cc7c4f479afeb38
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2025-11-08 21:58:53 -0300

    wip: progresso na prova para todo alfabeto

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
+- [ ] Finalizar prova para o caso zero e pensar na generalização na parte das macros. Mudar noção de equivalente em posição? Como expandir os next_steps de forma genérica?
+
+Menos importantes
 - [ ] A mudança na definição de computação vai causar problemas nos exemplos, mas vai facilitar as provas dos teoremas. TODO: Mudar Exemplos / Criar equivalência da versão antiga para a nova? ... 
-- [ ] Lema do get_equiv_state
-- [ ] admits de cada equivalência.
--- theories/equivalence/LanguagesProperties.v
+++ theories/equivalence/LanguagesProperties.v
@@ -214,11 +214,72 @@ Theorem nat_implies_string :
              p_str
              (StringLang.compute_program p_str (StringLang.SNAP 0 state_str) n').
 Proof.
-  intros p_nat state_nat k p_str simulated_prf.
-  exists (get_equiv_state k state_nat). induction n.
-  - exists 0. split.
-    + apply get_equiv_state_correct.
-      (* tem um admit no equiv_pos_simulated_0, mas é trivial
-         basta dizer que a macro nunca será vazia *)
+  intros p_nat state_nat k p_str simulated_prf. exists (get_equiv_state k state_nat).
+  induction n.
+    (* Caso Base:
+
+       Para mostramos prog_equiv, precisamos mostrar 1) state_equiv e 2) equiv_pos. 
+
+       1) Fazendo n' = 0, temos que em zero passos o programa retorna a própria snapshot.
+       Assim, basta mostrar que (get_equiv_state state_nat) é equivalente a state_nat.
+
+       2) Com n' = 0 e n = 0, a snapshot retornará (S, 0). Assim, precisamos mostrar que
+       se nth_error 0 prg é alguma instrução, então o programa de strings possui a macro
+       na posição n' (zero). O que é verdade pela hipótese simulated_prf.
+     *)
+  - exists 0. (* n' = 0 *)
+    split. (* Dividir a prova em 1) e 2) *)
+    + apply get_equiv_state_correct. 
     + apply equiv_pos_simulated_0, simulated_prf. 
- 
+
+
+  (* Passo da Indução:
+    Temos que existe um n' onde prog_equiv p_nat snap1 n
+                                           p_str snap2 n'
+
+     Precisamos saber qual a próxima instrução de n. Assim, vamos quebrar
+     a snap1 e a snap2. *)
+  - destruct IHn as [n']. (* Obtendo n' *)
+    (* Vendo os valores da snap1 e snap2 *)
+    destruct (NatLang.compute_program p_nat (NatLang.SNAP 0 state_nat) n)
+    as [i s] eqn:snap_nat. (* A próxima linha de p_nat é i *)
+    destruct (StringLang.compute_program p_str (StringLang.SNAP 0 
+    (get_equiv_state k state_nat)) n') as [i' s'] eqn:snap_str. 
+    (* A próxima linha de p_str é i' *)
+    destruct H. (* Quebramos a hipótese de indução em duas afirmações:
+                   1) Os estados após a computação são equivalentes.
+                   2) A próxima linha a ser executada em p_nat é ...
+                      e em p_str é ... *)
+
+    (* Para esse caso, o número de passos para o programa
+       de strings sera S n' para o passo da indução,  mas, em
+       casos genéricos, precisamos primeiro ver quem é a nth_error k de p_nat. *)
+
+    unfold equiv_pos in H0. 
+    destruct (nth_error p_nat i) eqn:E. (* Quem é a próxima instrução de nat? *)
+    (* nth_error p_nat i = Some i *)
+    (* Aqui, temos que nth_error p_str k' é Some (get_str_macro0 i).
+       No caso genérico, a alternativa que pensei é a seguinte:
+       1. skipn k' = [get_str_macro0 i] ++ algo, daí podemos
+          derivar que nth_error k' + 1 ... Até o tamanho total da macro.
+
+          Aqui está o lemma útil:
+
+            nth_error_skipn:
+              forall [A : Type] (n : nat) (l : list A) (i : nat),
+              nth_error (skipn n l) i = nth_error l (n + i)  *)
+    (* Qual a instrução e o statement? *)
+    + destruct i0 as [opt_lbl statement]. destruct statement.
+      (* x <- x + 1*)
+      ++ exists (n + n'). (* Para o caso do alfabeto qualquer, é simples,
+                             já que eu tenho um número limitado de next
+                             steps. Aqui eu preciso pensar em uma forma
+                             inteligente de separar a execução dos n
+                             (* n aqui é temporário, no teorema final será
+                                o número de execuções da macro *)
+                             passos dos n' passos *)
+         unfold prog_equiv. simpl. rewrite snap_nat.
+         unfold NatLang.next_step. rewrite E.
+Abort.
+
+           
