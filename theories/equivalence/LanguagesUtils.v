1ef7d0f898a85e51946ebdb9c9058a3fdb68b62c
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2026-06-21 17:16:16 -0300

    incr and decr function

--- theories/equivalence/LanguagesUtils.v
+++ theories/equivalence/LanguagesUtils.v
@@ -14,7 +14,7 @@ Import ListNotations.
 
 
 (* n = 2
-  [0, 1, 2] -> 3 elementos
+   [0, 1, 2] -> 3 elementos
    [1, 0, 0, 2, 1]
    2 * 3^4 + 1 * 3 *)
 
@@ -31,173 +31,31 @@ Definition string_to_nat (s : StringLang.string) (base : nat) :=
 
 (* Função de Incremento *)
 
-Fixpoint incr_string (s : StringLang.string) (base : nat) :=
+Fixpoint incr_string (s : StringLang.string) (max_char : nat) :=
   match s with 
-  | h :: t => if (h <? base) then (h + 1) :: t
-              else 0 :: incr_string t base 
+  | h :: t => if (h <? max_char) then (h + 1) :: t
+              else 0 :: incr_string t max_char 
   | [] => [0]
   end.
 
 
 (* Nat para String, basta usar o incremento n vezes *)
 
-Fixpoint nat_to_string (n : nat) (base : nat) : (StringLang.string)  :=
+Fixpoint nat_to_string (n : nat) (max_char : nat) : (StringLang.string)  :=
     match n with
-    | S n' => incr_string (nat_to_string n' base) base
     | 0 => []
+    | S n' => incr_string (nat_to_string n' max_char) max_char
     end.
 
 
-(** MACROS **)
+(* Decrementar String *)
 
-(** LEMAS E AUXILIARES **)
-
-Lemma Sn_leq_n'_implies_n_leq_n' : forall n n', S n <= n' -> n <= n'.
-Proof.
-  lia.
-Qed.
-
-(* rascunho ...[opt_lbl] IF X ENDS Si GOTO Ai ( 0 <= i <= n) *)
-
-
-(* TODO: Possivelmente vou ter que tomar cuidado com as labels.
-         Preciso garantir que as labels usadas pelas macros auxiliares
-         nunca ocorram dentro de p_nat. Uma opção é criar uma nova letra,
-         B, onde o programa em nat só usa A 1, A 2, A 3... E o programa
-         em strings pode usar B 1, ... B 2 livremente. *)
-(*
-Definition get_if_ends_macro (n : nat) (opt_lbl : option label)
-  (x : variable) : (StringLang.program n).
-Proof.
-  refine( 
-    let fix aux (k : nat) (k_leq_n : k <= n) :=
-      ((match k as eq return (k = eq) -> _ with 
-    | S n' => fun _ => 
-              let statement := 
-                 StringLang.IF_ENDS_GOTO x (StringLang.Char _ k k_leq_n) (Some (A n)) in 
-              (StringLang.Instr opt_lbl) statement :: aux n' _
-    | 0 =>  fun _ => let statement := 
-              StringLang.IF_ENDS_GOTO x (StringLang.Char _ k k_leq_n) (Some (A n)) in 
-              [(StringLang.Instr opt_lbl) statement]
-    end) eq_refl )
-    in aux (n) (le_n n )).
-    + assert (S n' <= n). { rewrite <- e. exact k_leq_n. }
-        apply Sn_leq_n'_implies_n_leq_n', H.
-Defined.
-
-
-(** PRINCIPAIS **)
-
-(* [opt_lbl] IF x GOTO l *)
-
-Definition get_if_macro (n : nat) (opt_lbl : option label)
-  (x : variable) (l : option label) : (StringLang.program n).
-Proof.
-  refine( 
-    let fix aux (k : nat) (k_leq_n : k <= n) :=
-      ((match k as eq return (k = eq) -> _ with 
-    | S n' => fun _ => 
-              let statement := 
-                 StringLang.IF_ENDS_GOTO x (StringLang.Char _ k k_leq_n) l in 
-              (StringLang.Instr opt_lbl) statement :: aux n' _
-    | 0 =>  fun _ => let statement := 
-              StringLang.IF_ENDS_GOTO x (StringLang.Char _ k k_leq_n) l in 
-              [(StringLang.Instr opt_lbl) statement]
-    end) eq_refl )
-    in aux (n) (le_n n )).
-    + assert (S n' <= n). { rewrite <- e. exact k_leq_n. }
-        apply Sn_leq_n'_implies_n_leq_n', H.
-Defined.
-
-(* [B0] x <- + 1 *)
-
-(* [B0] IF X ENDS si GOTO Ai    (1 <= i <= n)
-        Y <- s1 Y
-        GOTO E
-
-   [Ai] X <- X-       }
-        Y <- Si+1 Y    } (1 <= i <= n)
-        GOTO C1       }
-
-   [An] X <- X-
-        Y <- s1 Y
-        GOTO B0
-
-   [C1] IF X ENDS Si GOTO Di     (i <= i <= n)
-        GOTO E
-
-   [Di] X <- X-       }
-        Y <- Si Y      } (i <= i <= n)
-        GOTO C1       }                         *)
-
-
-Definition get_incr_macro (n : nat) (opt_lbl : option label)
-  (x : variable) : (StringLang.program n).
-Proof.
-    refine (
-    let if_x_ends_si_goto_ai := get_if_ends_macro n opt_lbl x
-    
-    in []
-    ).
-Defined.
-
-(* [opt_lbl] x <- - 1 *)
-
-
-(* [B0] IF X ENDS si GOTO Ai    (1 <= i <= n) 
-        GOTO E
-
-   [Ai] X <- X-       }
-        Y <- Si-1 Y    } (i < i <= n)
-        GOTO C        }
-
-   [A1] X <- X-
-        IF X != 0 GOTO C2
-        GOTO E
-
-   [C2] Y <- Sn Y
-        GOTO B0
-
-   [C1] IF X ENDS Si GOTO Di     (i <= i <= n)
-        GOTO E
-
-   [Di] X <- X-       }
-        Y <- Si Y      } (i <= i <= n)
-        GOTO C        }                         *)
-
-*)
-
-(*
-Definition get_decr_macro (n : nat) (opt_lbl : option label)
-  (x : variable)  : (StringLang.program n).
-Proof.
-Admitted.
-
-
-Lemma list_eq_last_init : forall {A: Type} (l: list A),
-  l <> [] -> exists init last, l = init ++ [last].
-Proof.
-  intros A l H.
-  induction l as [|h t IH].
-  - contradiction.
-  - destruct t as [|h' t'].
-    + exists [], h.  reflexivity.
-    + assert (h' :: t' <> []).
-      intros falso. inversion falso.
-      apply IH in  H0. destruct H0 as [init last].
-      destruct last.
-      exists (h :: init). exists x. rewrite <- app_comm_cons. f_equal.
-      assumption.
-Qed.
-
-
-(** Obter macros. Por enquanto está incompleta, falta definir INCR e o DECR *)
-
-Definition get_str_macro (k : nat) (i_nat : NatLang.instruction) :
-  (StringLang.program k) := 
-  match i_nat with 
-  | NatLang.Instr opt_lbl (NatLang.INCR x) => get_incr_macro k opt_lbl x
-  | NatLang.Instr opt_lbl (NatLang.DECR x) =>  get_decr_macro k opt_lbl x
-  | NatLang.Instr opt_lbl (NatLang.IF_GOTO x l) => get_if_macro k opt_lbl x l
-  end.
-*)
+Fixpoint decr_string (s: StringLang.string) (max_char : nat ) : (StringLang.string) :=
+match s with 
+| [ ]    => []
+| h :: t => match t with 
+            | [] => if h =? 0 then [] else [h + 1]
+            |  _ => if 0 <? h then (h - 1) :: t
+            else max_char :: decr_string t max_char
+            end
+end.
